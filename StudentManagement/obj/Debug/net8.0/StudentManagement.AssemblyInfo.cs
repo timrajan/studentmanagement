b1a5756d408549aa54eb3e2c1d5b7c9cf53a723a
timrajan <timothyrajan@outlook.com>
2025-11-03 10:09:04 +1100

    latest

--- StudentManagement/obj/Debug/net8.0/StudentManagement.AssemblyInfo.cs
+++ StudentManagement/obj/Debug/net8.0/StudentManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0299aa177ec675f0b21b46661d89730d5ae307b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb398753b539747cbb672deb054956745c7786e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
